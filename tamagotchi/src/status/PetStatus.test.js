fad6b722504027736864227d96da8bcac1bca0d6
Ilya Kushlianski <kushliansky@gmail.com>
2022-03-29 01:22:17 +0400

    Replace render destructuring with screen methods in tests
    Signed-off-by: Ilya Kushlianski <kushliansky@gmail.com>

--- tamagotchi/src/status/PetStatus.test.js
+++ tamagotchi/src/status/PetStatus.test.js
@@ -1,14 +1,14 @@
 import { possibleStatuses } from "./possibleStatuses";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { PetStatus } from "./PetStatus";
 
 describe("Pet Status", () => {
   it('should display status "fine" if pet status is fine', () => {
     const status = possibleStatuses.fine;
 
-    const { getByText } = render(<PetStatus status={status} />);
+    render(<PetStatus status={status} />);
 
-    const el = getByText("Status: fine");
+    const el = screen.getByText("Status: fine");
 
     expect(el).toBeInTheDocument();
   });
@@ -16,9 +16,9 @@ describe("Pet Status", () => {
   it('should display status "hungry" if pet is hungry', () => {
     const status = possibleStatuses.hungry;
 
-    const { getByText } = render(<PetStatus status={status} />);
+    render(<PetStatus status={status} />);
 
-    const el = getByText("Status: hungry");
+    const el = screen.getByText("Status: hungry");
 
     expect(el).toBeInTheDocument();
   });
@@ -26,9 +26,9 @@ describe("Pet Status", () => {
   it('should display status "sick" if pet is sick', () => {
     const status = possibleStatuses.sick;
 
-    const { getByText } = render(<PetStatus status={status} />);
+    render(<PetStatus status={status} />);
 
-    const el = getByText("Status: sick");
+    const el = screen.getByText("Status: sick");
 
     expect(el).toBeInTheDocument();
   });
@@ -36,9 +36,9 @@ describe("Pet Status", () => {
   it('should display status "dead" if pet is dead', () => {
     const status = possibleStatuses.dead;
 
-    const { getByText } = render(<PetStatus status={status} />);
+    render(<PetStatus status={status} />);
 
-    const el = getByText("Status: dead");
+    const el = screen.getByText("Status: dead");
 
     expect(el).toBeInTheDocument();
   });
@@ -46,9 +46,9 @@ describe("Pet Status", () => {
   it('should display status "unknown" if pet status does not match the pre-determined statuses', () => {
     const status = possibleStatuses.anything;
 
-    const { getByText } = render(<PetStatus status={status} />);
+    render(<PetStatus status={status} />);
 
-    const el = getByText("Status: unknown");
+    const el = screen.getByText("Status: unknown");
 
     expect(el).toBeInTheDocument();
   });
