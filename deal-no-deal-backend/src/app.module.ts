6856d095d5aa6b381f5a3d140bb98d8f5e1031f8
Ilya Kushlianski <kushliansky@gmail.com>
2022-04-19 20:41:49 +0400

    Generate basic entities
    Signed-off-by: Ilya Kushlianski <kushliansky@gmail.com>

--- deal-no-deal-backend/src/app.module.ts
+++ deal-no-deal-backend/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DbModule } from './db/db.module';
+import { UserModule } from './user/user.module';
+import { GameController } from './game/game.controller';
+import { GameService } from './game/game.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [DbModule, UserModule],
+  controllers: [AppController, GameController],
+  providers: [AppService, GameService],
 })
 export class AppModule {}
