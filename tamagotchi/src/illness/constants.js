8c59c953560e29e3afe962a63095e973268efdf8
Ilya Kushlianski <kushliansky@gmail.com>
2022-04-23 01:39:40 +0400

    Set proper sick-to-death timeout as per requirements

--- tamagotchi/src/illness/constants.js
+++ tamagotchi/src/illness/constants.js
@@ -1,4 +1,3 @@
-// todo replace with 15 seconds as per requirements
-export const TIME_FROM_SICK_TO_DEATH_MS = 5000;
+export const TIME_FROM_SICK_TO_DEATH_MS = 15000;
 
 export const HOW_OFTEN_TO_CHECK_SICKNESS_MS = 700;
