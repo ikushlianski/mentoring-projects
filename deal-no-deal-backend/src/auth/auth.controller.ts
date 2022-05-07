b153eb85f6121df30c7eeedcf33ed11e3284161d
Ilya Kushlianski <kushliansky@gmail.com>
2022-05-07 12:33:20 +0300

    Add @nestjs/swagger package
    Signed-off-by: Ilya Kushlianski <kushliansky@gmail.com>

--- deal-no-deal-backend/src/auth/auth.controller.ts
+++ deal-no-deal-backend/src/auth/auth.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Post, UnauthorizedException } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { SignUpUserDto } from 'src/auth/dto/signUpUserDto';
 import { AuthService } from './auth.service';
 
@@ -7,12 +8,18 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('cognito/signup')
+  @ApiTags('signup')
+  @ApiOperation({ description: 'Sign up (create) the user' })
   signUp(@Body() signUpUserDto: SignUpUserDto) {
     try {
       console.log('signUpUserDto', signUpUserDto);
       // return this.authService.signUpWithCognito(signUpUserDto);
     } catch (error: unknown) {
       console.log('Error during sign up', error);
+
+      if (error instanceof Error) {
+        throw new UnauthorizedException(error.message);
+      }
 
       throw new UnauthorizedException('Error occurred when signing you up');
     }
