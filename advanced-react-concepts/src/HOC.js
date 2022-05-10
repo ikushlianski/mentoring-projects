e896ce39b840665ea940bddf8fd5727dc16482de
Ilya Kushlianski <kushliansky@gmail.com>
2022-05-10 14:39:07 +0300

    Add HOC and HOF example
    Signed-off-by: Ilya Kushlianski <kushliansky@gmail.com>

---
+++ advanced-react-concepts/src/HOC.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+export const HOC = ({ text, icon = false }) => {
+  return (
+    <div>
+      {icon && <img />}
+      <button>{text}</button>
+    </div>
+  );
+};
+
+export default withAuth(HOC);
+
+// const LoginPage = () => {
+//   const [loggedIn, setLoggedIn] = useState(false)
+//
+//   return (
+//     <div>
+//       {loggedIn && <HOC icon={'icon'} text={'Log out'} />}
+//
+//     </div>
+//   );
+// };
+
+export const withAuth = async (WrappedComponent) => {
+  const result = fetch("/login", { method: "POST" });
+  const { loggedIn } = await result.json();
+
+  if (loggedIn) {
+    return <WrappedComponent text={"Log out"} newProps={"newnew"} />;
+  }
+
+  return <WrappedComponent text={"Log in"} icon={"please log in"} />;
+};
