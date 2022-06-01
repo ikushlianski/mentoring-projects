dbc1afbdf9ff6d01321f8b5726a5bdea5b3b5001
Ilya Kushlianski <kushliansky@gmail.com>
2022-06-01 21:08:31 +0300

    Move to single mongo instance instead of replica set

--- deal-no-deal-backend/IaC/docker/init-mongo.sh
+++ deal-no-deal-backend/IaC/docker/init-mongo.sh
@@ -2,53 +2,51 @@
 
 set -e
 
-mongo <<EOF1
-
-var config = {
-    "_id": "dbrs",
-    "version": 1,
-    "members": [
-        {
-            "_id": 1,
-            "host": "mongo1:27017",
-            "priority": 1
-        },
-        {
-            "_id": 2,
-            "host": "mongo2:27017",
-            "priority": 1
-        },
-        {
-            "_id": 3,
-            "host": "mongo3:27017",
-            "priority": 3
-        }
-    ]
-};
-
-rs.initiate(config);
-
-rs.status();
-
-EOF1
+#mongo <<EOF1
+#
+#var config = {
+#    "_id": "dbrs",
+#    "version": 1,
+#    "members": [
+#        {
+#            "_id": 0,
+#            "host": "mongo:27017",
+#            "priority": 1
+#        }
+#    ]
+#};
+#
+#rs.initiate(config);
+#
+#rs.status();
+#
+#EOF1
 
 echo "Preparing database..."
 
-sleep 15
+#sleep 3
 
 mongo <<EOF2
 
-use $MONGO_INITDB_DATABASE
+use admin
+
+db.createUser(
+    {
+        user: "ilya",
+        pwd: "pass",
+        roles: [ { role: "dbOwner", db: "admin"} ],
+        passwordDigestor: "server",
+    }
+)
 
 db.createCollection('user')
 
 db.user.insert(
   {
     username: "$TF_VAR_COGNITO_DEFAULT_USER_NAME",
-    role: 'admin',
+    role: 'somethingg',
     customprop: 'something-test'
-  },
-  { writeConcern: { w: "majority", wtimeout: 5000 } }
+  }
 );
 
 EOF2
