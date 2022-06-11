c7a9e951bde880a66c4903c8e0afb2e55ea174cf
Ilya Kushlianski <kushliansky@gmail.com>
2022-06-11 23:41:13 +0300

    TimeManager can now determine how many meals are left until the end of day

--- meal-tracker/src/time/TimeManager.test.ts
+++ meal-tracker/src/time/TimeManager.test.ts
@@ -1,36 +1,96 @@
-import { MealStorage } from "src/meal-list/meal-storage";
+import { Meal } from "src/meal/meal";
+import {
+  appSettingsLocalStorageKey,
+  ConfigurableAppSettings,
+} from "src/settings/constants";
+import { SettingsManager } from "src/settings/settingsManager";
 import { TimeManager } from "src/time/TimeManager";
 
 describe("TimeManager", () => {
   describe("isLongPastLastMeal", () => {
+    afterEach(() => {
+      jest.resetAllMocks();
+    });
+
     const mockMeals = [
       // ...earlier meals
       { time: new Date(Date.parse("Jun 11 2022 17:01:10")) },
       { time: new Date(Date.parse("Jun 11 2022 21:43:01")) },
-    ];
+    ] as Meal[];
 
-    const mockMealStorage = {
-      getMealList: () => mockMeals,
-    } as MealStorage;
+    const settingsManager = new SettingsManager();
 
     it("should return true if now is more than 4 hours after the last meal", () => {
       const mockNow = new Date(Date.parse("Jun 12 2022 02:55:00"));
 
-      jest.spyOn(Date, "now").mockReturnValueOnce(+mockNow);
+      jest.spyOn(Date, "now").mockReturnValue(+mockNow);
 
-      const timeManager = new TimeManager(mockMealStorage);
+      const timeManager = new TimeManager(settingsManager);
 
-      expect(timeManager.isLongPastLastMeal()).toBe(true);
+      expect(timeManager.isLongPastLastMeal(mockMeals)).toBe(true);
     });
 
     it("should return false if less than 4 hours after passed since the last meal", () => {
       const mockNow = new Date(Date.parse("Jun 11 2022 23:18:39"));
 
-      jest.spyOn(Date, "now").mockReturnValueOnce(+mockNow);
+      jest.spyOn(Date, "now").mockReturnValue(+mockNow);
 
-      const timeManager = new TimeManager(mockMealStorage);
+      const timeManager = new TimeManager(settingsManager);
 
-      expect(timeManager.isLongPastLastMeal()).toBe(false);
+      expect(timeManager.isLongPastLastMeal(mockMeals)).toBe(false);
+    });
+  });
+
+  describe("How many meals fit in time remaining until midnight", () => {
+    beforeAll(() => {
+      localStorage.setItem(
+        appSettingsLocalStorageKey,
+        JSON.stringify({
+          [ConfigurableAppSettings.TimeFromWakeUpTillBreakfastMinutes]: 20,
+          [ConfigurableAppSettings.IntervalBetweenMealsMinutes]: 140,
+          [ConfigurableAppSettings.MealsPerDay]: 6,
+        })
+      );
+    });
+
+    afterEach(() => {
+      jest.resetAllMocks();
+    });
+
+    afterAll(() => {
+      localStorage.removeItem(appSettingsLocalStorageKey);
+    });
+
+    const settingsManager = new SettingsManager();
+
+    it("should say there are 2 meals remaining until the end of day", () => {
+      const mockNow = new Date(Date.parse("Jun 11 2022 19:55:01"));
+
+      jest.spyOn(Date, "now").mockReturnValue(+mockNow);
+
+      const timeManager = new TimeManager(settingsManager);
+
+      expect(timeManager.howManyMealsFitUntilDayEnds()).toBe(2);
+    });
+
+    it("should say there is one meal remaining until the end of day", () => {
+      const mockNow = new Date(Date.parse("Jun 10 2022 21:18:39"));
+
+      jest.spyOn(Date, "now").mockReturnValue(+mockNow);
+
+      const timeManager = new TimeManager(settingsManager);
+
+      expect(timeManager.howManyMealsFitUntilDayEnds()).toBe(1);
+    });
+
+    it("should say there are no meals remaining until the end of day", () => {
+      const mockNow = new Date(Date.parse("Jun 10 2022 22:30:39"));
+
+      jest.spyOn(Date, "now").mockReturnValue(+mockNow);
+
+      const timeManager = new TimeManager(settingsManager);
+
+      expect(timeManager.howManyMealsFitUntilDayEnds()).toBe(0);
     });
   });
 });
--- meal-tracker/src/time/TimeManager.ts
+++ meal-tracker/src/time/TimeManager.ts
@@ -1,21 +1,38 @@
 import dayjs from "dayjs";
-import { mealStorage, MealStorage } from "src/meal-list/meal-storage";
+import { Meal } from "src/meal/meal";
+import { settingsManager, SettingsManager } from "src/settings/settingsManager";
 
 export class TimeManager {
-  constructor(private mealStorage: MealStorage) {}
+  constructor(private settingsManager: SettingsManager) {}
 
-  public isLongPastLastMeal() {
-    // We mean "4 hrs after last meal"
+  public howManyMealsFitUntilDayEnds(): number {
+    const roughHoursTillMidnight = TimeManager.getApproxHoursTillMidnight();
+
+    const minutesBetweenMeals = this.settingsManager.getSetting(
+      "IntervalBetweenMealsMinutes"
+    );
+
+    return Math.round((roughHoursTillMidnight * 60) / minutesBetweenMeals);
+  }
+
+  public isLongPastLastMeal(list: Meal[]) {
     // When we say "long past", this means
     // we're most likely crossing into the next day
+
+    // We mean "4 hrs after last meal"
     const longPastThresholdHours = 4;
-    const list = this.mealStorage.getMealList();
     const lastMealTime = list[list.length - 1].time;
 
     return dayjs(Date.now()).isAfter(
       dayjs(lastMealTime).add(longPastThresholdHours, "hours")
     );
   }
+
+  private static getApproxHoursTillMidnight() {
+    const endOfDay = dayjs(Date.now()).endOf("day");
+
+    return Math.floor(endOfDay.diff(dayjs(Date.now()), "hours"));
+  }
 }
 
-export const timeManager = new TimeManager(mealStorage);
+export const timeManager = new TimeManager(settingsManager);
