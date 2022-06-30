1f2e06ac2daaa12fd3f016a6fb0dab7b4935e445
Ilya Kushlianski <kushliansky@gmail.com>
2022-06-30 12:22:30 +0300

    Add getBooking lambda that goes to DB

--- honda-tracker/src/booking/getBooking.ts
+++ honda-tracker/src/booking/getBooking.ts
@@ -1,9 +1,43 @@
-exports.handler = async function (event: any) {
-  console.log("request:", JSON.stringify(event, null, 2));
+import {
+  DynamoDBClient,
+  QueryCommand,
+  QueryCommandOutput,
+} from "@aws-sdk/client-dynamodb";
+import { marshall, unmarshall } from "@aws-sdk/util-dynamodb";
+import { AWS_REGION, TABLE_NAME_DEV } from "../constants";
 
-  return {
-    statusCode: 200,
-    headers: { "Content-Type": "text/plain" },
-    body: `Get booking will be implemented here`,
+const client = new DynamoDBClient({ region: AWS_REGION });
+
+exports.handler = async function (event: any) {
+  const input = {
+    TableName: TABLE_NAME_DEV,
+    KeyConditionExpression: "pk = :pk",
+    ExpressionAttributeValues: marshall({
+      ":pk": "user#papa",
+    }),
   };
+  const command = new QueryCommand(input);
+
+  try {
+    const { Items } = await client.send(command);
+
+    if (Items?.[0]) {
+      return {
+        statusCode: 200,
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(unmarshall(Items[0])),
+      };
+    }
+
+    return {
+      statusCode: 200,
+      body: `No items`,
+    };
+  } catch (error) {
+    console.log("ERror in getBooking Lambda", error);
+    return {
+      statusCode: 500,
+      body: `Error occurred`,
+    };
+  }
 };
