33e47ff8699729001f25a6516684bc479a54b476
Ilya Kushlianski <kushliansky@gmail.com>
2022-07-07 19:43:57 +0300

    Improve body handling when shaping Lambda response

--- honda-tracker/src/lambda/lambda.service.ts
+++ honda-tracker/src/lambda/lambda.service.ts
@@ -1,32 +1,41 @@
-import { APIGatewayProxyStructuredResultV2 } from "aws-lambda/trigger/api-gateway-proxy";
-import { StatusCodes } from "http-status-codes";
+import { APIGatewayProxyStructuredResultV2 } from 'aws-lambda/trigger/api-gateway-proxy';
+import { StatusCodes } from 'http-status-codes';
 
 export class LambdaService {
   toSuccessResponse = (
     body: unknown,
     headers = {},
-    code = StatusCodes.OK
+    // cookies = [],
+    code = StatusCodes.OK,
   ): APIGatewayProxyStructuredResultV2 => {
+    const isBodyString = (body: unknown): body is string => {
+      return typeof body === 'string';
+    };
+
     return {
       statusCode: code,
       headers,
-      body: JSON.stringify(body),
+      // cookies,
+      body: isBodyString(body) ? body : JSON.stringify(body),
     };
   };
 
   toErrorResponse = (
     error: unknown,
-    code: StatusCodes
+    code: StatusCodes,
+    // cookies = [],
   ): APIGatewayProxyStructuredResultV2 => {
     return error instanceof Error
       ? {
           statusCode: code,
           body: error.message,
+          // cookies,
         }
       : {
           // should not happen
-          statusCode: 500,
-          body: "Unknown error",
+          statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
+          body: 'Unknown error',
+          // cookies,
         };
   };
 }
