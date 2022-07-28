d57aa896fe0856afbb117a3620c387f9c57db8c8
Ilya Kushlianski <kushliansky@gmail.com>
2022-07-28 22:18:01 +0300

    Add ts-based seed script for dev env

---
+++ honda-tracker/src/db/seeds/defaultData_dev.ts
@@ -0,0 +1,36 @@
+import { HondaTrackerDynamoService } from '../db.service';
+import { FAMILY_HONDA_CAR_NUMBER } from '../../car/car.constants';
+import { UserRoles } from '../../user/user.constants';
+
+(async () => {
+  // cars
+  const insertCarHonda = HondaTrackerDynamoService.entities.car
+    .create({
+      carId: FAMILY_HONDA_CAR_NUMBER,
+      username: 'owner#papa',
+    })
+    .go();
+
+  // users
+  const insertUserIlya = HondaTrackerDynamoService.entities.user
+    .create({
+      username: 'ilya',
+      password: process.env.ILYA_PASSWORD as string,
+      roles: [UserRoles.DRIVER],
+      availableCarIds: [FAMILY_HONDA_CAR_NUMBER],
+    })
+    .go();
+
+  const insertUserPapa = HondaTrackerDynamoService.entities.user
+    .create({
+      username: 'papa',
+      password: process.env.PAPA_PASSWORD as string,
+      roles: [UserRoles.CAR_PROVIDER, UserRoles.DRIVER],
+      availableCarIds: [FAMILY_HONDA_CAR_NUMBER],
+    })
+    .go();
+
+  await Promise.all([insertCarHonda, insertUserIlya, insertUserPapa]);
+
+  console.log('done');
+})();
