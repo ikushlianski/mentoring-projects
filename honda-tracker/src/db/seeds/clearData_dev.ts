999a1150eac911768f2698b5424f978a7df22722
Ilya Kushlianski <kushliansky@gmail.com>
2022-07-29 13:36:40 +0300

    Add script to clear dev data

---
+++ honda-tracker/src/db/seeds/clearData_dev.ts
@@ -0,0 +1,21 @@
+import { FAMILY_HONDA_CAR_NUMBER } from '../../car/car.constants';
+import { HondaTrackerDynamoService } from '../db.service';
+
+(async () => {
+  await Promise.all([
+    // remove users
+    HondaTrackerDynamoService.entities.user
+      .delete({ username: 'papa' })
+      .go(),
+    HondaTrackerDynamoService.entities.user
+      .delete({ username: 'ilya' })
+      .go(),
+
+    // remove cars
+    HondaTrackerDynamoService.entities.car
+      .delete({ carId: FAMILY_HONDA_CAR_NUMBER })
+      .go(),
+
+    // remove bookings
+  ]);
+})();
